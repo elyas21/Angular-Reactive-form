ed86febacaa507190e1e242efa1a09ff9d6c9d84
unknown <elyasabate21@gmail.com>
2023-02-01 18:24:34 +0300

    cross validation added

--- src/app/com/form-validator/form-validator.component.ts
+++ src/app/com/form-validator/form-validator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-validator',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-validator.component.scss']
 })
 export class FormValidatorComponent {
-
+  registerForm = new FormGroup({
+    'name': new FormControl(),
+    'password': new FormControl('',[Validators.required]),
+    'confPassword': new FormControl()
+  },{ validators: passwordMatchValidator });
+  onSubmit(){
+    console.log(this.registerForm.value);
+    
+  }
 }
+
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password:string = control.get('password')?.value;
+  const confPassword = control.get('confPassword')?.value;
+if(password && confPassword)
+return password.toLowerCase() != confPassword.toLowerCase() ? { diff: true } : null;
+else return {diff: true}
+};
