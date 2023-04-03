2eacb6b0446c5b86a31470d2924437816146a2ec
elyas <elyasabate21@gmail.com>
2023-04-03 10:03:16 +0300

    nested form array

--- src/app/com/nest-form-group/nest-form-group.component.ts
+++ src/app/com/nest-form-group/nest-form-group.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nest-form-group',
   templateUrl: './nest-form-group.component.html',
-  styleUrls: ['./nest-form-group.component.scss']
+  styleUrls: ['./nest-form-group.component.scss'],
 })
 export class NestFormGroupComponent {
   userForm = new FormGroup({
+    closeRelatives: new FormGroup({
+      familyMembers: new FormArray([new FormControl('')]),
+      friends: new FormArray([
+        new FormGroup({
+          name: new FormControl(),
+          rank: new FormControl(),
+        }),
+      ]),
+    }),
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
@@ -16,18 +25,34 @@ export class NestFormGroupComponent {
       city: new FormControl(''),
       zip: new FormControl(''),
     }),
-    addressTwo: this.addressTwoGenrartor()
-  })
+    addressTwo: this.addressTwoGenrartor(),
+  });
 
-  onSave(){
+  onSave() {
     console.log(this.userForm.value);
   }
-  addressTwoGenrartor(){
+  addressTwoGenrartor() {
     return new FormGroup({
       country: new FormControl(''),
       city: new FormControl(''),
       zip: new FormControl(''),
     });
   }
+
+  closeRelatives(name: string): FormArray {
+    return this.userForm.get(['closeRelatives', name]) as FormArray;
+  }
+
+  addFamilyMembers() {
+    this.closeRelatives('familyMembers').push(new FormControl());
+  }
+
+  addFriends() {
+    this.closeRelatives('friends').push(
+      new FormGroup({
+        name: new FormControl(),
+        rank: new FormControl(),
+      })
+    );
+  }
 }
- 
